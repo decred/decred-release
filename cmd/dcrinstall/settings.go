e9d43438e8fe4f8f5302eb43b3aa6d977b5be7c2
John C. Vernaleo <john@netpurgatory.com>
2016-08-10 15:17:45 -0400

    Change defaults for v0.3.0

--- cmd/dcrinstall/settings.go
+++ cmd/dcrinstall/settings.go
@@ -14,8 +14,8 @@ import (
 
 // latestVersion and latestManifest must be updated every release.
 const (
-	latestManifest = "manifest-v0.2.0.txt"
-	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v0.2.0"
+	latestManifest = "manifest-v0.3.0.txt"
+	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v0.3.0"
 
 	netMain  = "mainnet"
 	netTest  = "testnet"
