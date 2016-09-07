c4b4d605eaf3dbdf7baf01b81d4ebf58961a4fd3
jolan <jolan@users.noreply.github.com>
2016-09-07 08:19:32 -0500

    remove last reference to webui (#59)

--- cmd/dcrinstall/dcrinstall.go
+++ cmd/dcrinstall/dcrinstall.go
@@ -297,8 +297,7 @@ func (c *ctx) copy(version string) error {
 
 	// non binaries
 	filename := filepath.Join(c.s.Destination,
-		"decred-"+c.s.Tuple+"-"+version,
-		"webui")
+		"decred-"+c.s.Tuple+"-"+version)
 
 	c.log("installing: %v -> %v\n", filename, c.s.Destination)
 
