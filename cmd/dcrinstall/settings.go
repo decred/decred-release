8a3c8f05287f8a44db9afaed0b9a8608429a3afb
John C. Vernaleo <john@netpurgatory.com>
2017-06-07 13:13:59 -0400

    Bump for v1.0.3

--- cmd/dcrinstall/settings.go
+++ cmd/dcrinstall/settings.go
@@ -17,8 +17,8 @@ import (
 
 // latestVersion and latestManifest must be updated every release.
 const (
-	latestManifest = "manifest-v1.0.1.txt"
-	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.0.1"
+	latestManifest = "manifest-v1.0.3.txt"
+	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.0.3"
 
 	netMain  = "mainnet"
 	netTest  = "testnet"
