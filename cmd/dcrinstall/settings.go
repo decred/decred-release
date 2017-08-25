15d1f608e21aa1afbd05689a2454dbe49af294e8
jolan <jolan@users.noreply.github.com>
2017-08-25 12:51:59 -0500

    bump for 1.0.8 and fix sample-dcrd.conf handling (#124)

--- cmd/dcrinstall/settings.go
+++ cmd/dcrinstall/settings.go
@@ -17,8 +17,8 @@ import (
 
 // latestVersion and latestManifest must be updated every release.
 const (
-	latestManifest = "manifest-v1.0.7.txt"
-	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.0.7"
+	latestManifest = "manifest-v1.0.8.txt"
+	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.0.8"
 
 	netMain  = "mainnet"
 	netTest  = "testnet"
