791378dc986a50871d7d29682b5bdc16cc7447cf
David Hill <dhill@mindcry.org>
2018-04-26 16:07:07 -0400

    update version

--- cmd/dcrinstall/settings.go
+++ cmd/dcrinstall/settings.go
@@ -17,8 +17,8 @@ import (
 
 // latestVersion and latestManifest must be updated every release.
 const (
-	latestManifest = "manifest-v1.2.0-rc1.txt"
-	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.2.0-rc1"
+	latestManifest = "manifest-v1.2.0"
+	defaultURI     = "https://github.com/decred/decred-binaries/releases/download/v1.2.0"
 
 	netMain  = "mainnet"
 	netTest  = "testnet"
