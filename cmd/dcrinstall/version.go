47650751b483bd41bb5e7b946b55e96e3d464540
David Hill <dhill@mindcry.org>
2020-08-28 16:20:08 +0000

    prepare 1.5.2  (#175)
    * build: test go 1.15; update golangci-lint
    
    * prepare 1.5.2

--- cmd/dcrinstall/version.go
+++ cmd/dcrinstall/version.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2019 The Decred developers
+// Copyright (c) 2015-2020 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -19,7 +19,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 1
 	appMinor uint = 5
-	appPatch uint = 1
+	appPatch uint = 2
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
