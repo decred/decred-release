fd6196d1a8a48e5b6bb004a0a2377a589cf6cb75
Marco Peereboom <marco@peereboom.us>
2020-11-03 14:21:09 -0600

    Give binary files same error prtin treatment as config files (#194)

--- cmd/dcrinstall/dcrdex.go
+++ cmd/dcrinstall/dcrdex.go
@@ -95,6 +95,7 @@ func preconditionsDcrdexInstall() error {
 	currentlyInstalled := 0
 	expectedInstalled := 0
 	currentVersion := make(map[string][]string)
+	var installedBins, notInstalledBins []string
 	for k := range dexf {
 		filename := filepath.Join(destination, dexf[k].Name)
 
@@ -109,6 +110,7 @@ func preconditionsDcrdexInstall() error {
 		version, err := cmd.CombinedOutput()
 		if err != nil {
 			log.Printf("Currently not installed: %v", dexf[k].Name)
+			notInstalledBins = append(notInstalledBins, filename)
 			continue
 		}
 		v, err := extractSemVer(string(version))
@@ -120,27 +122,16 @@ func preconditionsDcrdexInstall() error {
 		log.Printf("Version installed %v: %v", dexf[k].Name, ver)
 		currentlyInstalled++
 		currentVersion[ver] = append(currentVersion[ver], dexf[k].Name)
+		installedBins = append(installedBins, filename)
 	}
-
-	// XXX this is commented out because we mix versions.
-	// Reject mixed versions
-	//if len(currentVersion) > 1 {
-	//	return fmt.Errorf("Multiple versions of binaries found; " +
-	//		"upgrade requires human intervention")
-	//}
-
-	// If the current version is already installed error out.
-	//for k := range currentVersion {
-	//	log.Printf("k %v", k)
-	//	// XXX check manifest version against currentVersion
-	//	panic("fixme")
-	//}
 
 	// Determine if everything or nothing is installed
 	if currentlyInstalled != 0 && currentlyInstalled != expectedInstalled {
-		return fmt.Errorf("Currently installed binaries does not " +
-			"match expected installed binaries; upgrade requires " +
-			"human intervention")
+		return fmt.Errorf("dcrinstall requires all or none of the "+
+			"binary files to be installed. This is "+
+			"to prevent improper installations or upgrades. This "+
+			"upgrade/install requires human intervention.\n\n%v",
+			printConfigError(installedBins, notInstalledBins))
 	}
 
 	// Install config files if applicable
@@ -168,7 +159,7 @@ func preconditionsDcrdexInstall() error {
 
 	if currentConfigFiles != 0 && currentConfigFiles != expectedConfigFiles {
 		return fmt.Errorf("dcrinstall requires all or none of the "+
-			"configuration files files to be installed. This is "+
+			"configuration files to be installed. This is "+
 			"to prevent improper installations or upgrades. This "+
 			"upgrade/install requires human intervention.\n\n%v",
 			printConfigError(installedConfigs, notInstalledConfigs))
